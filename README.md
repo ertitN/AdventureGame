88b0ce643b7c6be8239c348ccb0a607f69b2fe43
Nedim Ertit <69191550+ertitN@users.noreply.github.com>
2023-01-30 11:39:21 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+patika.dev kapsamında yaptığım macera oyunu projesi
---
+++ src/Deneme.java
@@ -0,0 +1,15 @@
+public class Deneme {
+    public static void main(String[] args) {
+
+        Player player = new Player("Nedim");
+        Mine mine = new Mine(player);
+
+
+
+
+
+
+
+
+    }
+}
